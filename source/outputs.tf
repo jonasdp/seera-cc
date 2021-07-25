eaceab2eb633f6c25dc18e5797af25672d0735b2
Jonas Persson <jonas.persson@inter.ikea.com>
2021-07-25 23:19:03 +0200

    bugfix

--- source/outputs.tf
+++ source/outputs.tf
@@ -1,3 +1,3 @@
 output "db_access" {
-  value = "mysql -h ${aws_db_instance.this.address} -P ${aws_db_instance.this.port} -u ${var.db_user} -p${var.db_password}"
+  value = "mysql -h ${aws_db_instance.this.address} -P ${aws_db_instance.this.port} -u ${var.db_user} -p ${var.db_password}"
 }
