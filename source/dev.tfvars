2b4b91e89143ec885853264a118e7fca76251b61
Jonas Persson <jonas.persson@inter.ikea.com>
2021-07-25 21:45:48 +0200

    only alphas in db name bugfix

--- source/dev.tfvars
+++ source/dev.tfvars
@@ -1,6 +1,6 @@
 aws_region      = "eu-west-1"
 project_name    = "seera-cc"
-db_name         = "seera-db"
+db_name         = "seeradb"
 db_user         = "user"
 db_password     = "passw0rd"
 ssh_public_key  = "./id_seera.pub"
--- source/outputs.tf
+++ source/outputs.tf
@@ -1,5 +1,3 @@
-
-output "rds_hostname" {
-  description = "RDS instance hostname"
-  value       = aws_db_instance.this.address
+output "db_access" {
+  value = "mysql -h ${aws_db_instance.this.address} -P ${aws_db_instance.this.port} -u ${var.db_user} -p${var.db_password}"
 }
